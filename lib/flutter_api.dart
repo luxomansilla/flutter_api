c0907289f9dabb9a9590da8ec3784eb4b2cfc94d
Luxo <luxomansilla@gmail.com>
2021-04-03 15:29:29 -0300

    formatter

--- lib/flutter_api.dart
+++ lib/flutter_api.dart
@@ -1,11 +1,9 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class FlutterApi {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_api');
+  static const MethodChannel _channel = const MethodChannel('flutter_api');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
