46f59b3a04dd5671043c1a2d706c78decf09d09b
Luxo <luxomansilla@gmail.com>
2021-04-03 12:20:39 -0300

    api builders

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_api/api.dart';
-import 'package:flutter_api/responses.dart';
+import 'package:flutter_api/api_builders.dart';
 
 void main() {
   runApp(MyApp());
@@ -143,115 +143,5 @@ class _MyAppState extends State<MyApp> {
         ],
       ),
     );
-  }
-}
-
-class ApiBuilder<T> extends StatelessWidget {
-  final Widget Function(ApiResponse<T> data) builder;
-  final Widget loading;
-  final String url;
-  final Map<String, String> args;
-  final String method;
-  final String dataPath;
-  final String errorPath;
-  final bool showLoading;
-  ApiBuilder(
-      {@required this.url,
-      this.args,
-      this.method,
-      this.dataPath,
-      @required this.builder,
-      this.loading,
-      this.showLoading,
-      this.errorPath});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<ApiResponse<T>>(
-        future: Api.get<T>(url, args: args, method: method, dataPath: dataPath),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return builder(snapshot.data);
-          } else if (snapshot.connectionState == ConnectionState.waiting &&
-              showLoading) {
-            return loading ?? Center(child: CircularProgressIndicator());
-          } else if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.data.hasData) {}
-          return Container();
-        });
-  }
-}
-
-class ApiListBuilder<T> extends StatelessWidget {
-  final Widget Function(ApiResponseList<T> data) builder;
-  final Widget loading;
-  final String url;
-  final Map<String, String> args;
-  final String method;
-  final String dataPath;
-  final String errorPath;
-  final bool showLoading;
-  ApiListBuilder(
-      {@required this.url,
-      this.args,
-      this.method,
-      this.dataPath,
-      @required this.builder,
-      this.loading,
-      this.showLoading: true,
-      this.errorPath});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<ApiResponseList<T>>(
-        future:
-            Api.getList<T>(url, args: args, method: method, dataPath: dataPath),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return builder(snapshot.data);
-          } else if (snapshot.connectionState == ConnectionState.waiting &&
-              showLoading) {
-            return loading ?? Center(child: CircularProgressIndicator());
-          } else if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.data.hasData) {}
-          return Container();
-        });
-  }
-}
-
-class ApiMapBuilder<K, V> extends StatelessWidget {
-  final Widget Function(ApiResponseMap<K, V> data) builder;
-  final Widget loading;
-  final String url;
-  final Map<String, String> args;
-  final String method;
-  final String dataPath;
-  final String errorPath;
-  final bool showLoading;
-  ApiMapBuilder(
-      {@required this.url,
-      this.args,
-      this.method,
-      this.dataPath,
-      @required this.builder,
-      this.loading,
-      this.showLoading: true,
-      this.errorPath});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<ApiResponseMap<K, V>>(
-        future: Api.getMap<K, V>(url,
-            args: args, method: method, dataPath: dataPath),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return builder(snapshot.data);
-          } else if (snapshot.connectionState == ConnectionState.waiting &&
-              showLoading) {
-            return loading ?? Center(child: CircularProgressIndicator());
-          } else if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.data.hasData) {}
-          return Container();
-        });
   }
 }
---
+++ lib/api_builders.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import 'responses.dart';
+import 'api.dart';
+
+class ApiBuilder<T> extends StatelessWidget {
+  final Widget Function(ApiResponse<T> data) builder;
+  final Widget loading;
+  final String url;
+  final Map<String, String> args;
+  final String method;
+  final String dataPath;
+  final String errorPath;
+  final bool showLoading;
+  ApiBuilder(
+      {@required this.url,
+      this.args,
+      this.method,
+      this.dataPath,
+      @required this.builder,
+      this.loading,
+      this.showLoading,
+      this.errorPath});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ApiResponse<T>>(
+        future: Api.get<T>(url, args: args, method: method, dataPath: dataPath),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return builder(snapshot.data);
+          } else if (snapshot.connectionState == ConnectionState.waiting &&
+              showLoading) {
+            return loading ?? Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.data.hasData) {}
+          return Container();
+        });
+  }
+}
+
+class ApiListBuilder<T> extends StatelessWidget {
+  final Widget Function(ApiResponseList<T> data) builder;
+  final Widget loading;
+  final String url;
+  final Map<String, String> args;
+  final String method;
+  final String dataPath;
+  final String errorPath;
+  final bool showLoading;
+  ApiListBuilder(
+      {@required this.url,
+      this.args,
+      this.method,
+      this.dataPath,
+      @required this.builder,
+      this.loading,
+      this.showLoading: true,
+      this.errorPath});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ApiResponseList<T>>(
+        future:
+            Api.getList<T>(url, args: args, method: method, dataPath: dataPath),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return builder(snapshot.data);
+          } else if (snapshot.connectionState == ConnectionState.waiting &&
+              showLoading) {
+            return loading ?? Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.data.hasData) {}
+          return Container();
+        });
+  }
+}
+
+class ApiMapBuilder<K, V> extends StatelessWidget {
+  final Widget Function(ApiResponseMap<K, V> data) builder;
+  final Widget loading;
+  final String url;
+  final Map<String, String> args;
+  final String method;
+  final String dataPath;
+  final String errorPath;
+  final bool showLoading;
+  ApiMapBuilder(
+      {@required this.url,
+      this.args,
+      this.method,
+      this.dataPath,
+      @required this.builder,
+      this.loading,
+      this.showLoading: true,
+      this.errorPath});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ApiResponseMap<K, V>>(
+        future: Api.getMap<K, V>(url,
+            args: args, method: method, dataPath: dataPath),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return builder(snapshot.data);
+          } else if (snapshot.connectionState == ConnectionState.waiting &&
+              showLoading) {
+            return loading ?? Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.data.hasData) {}
+          return Container();
+        });
+  }
+}
